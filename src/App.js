5432108db712b32fe408799798023c19a9ac4fd9
Avirup Saha <sahaavirup1998@gmail.com>
2021-05-16 11:03:00 +0530

    React Assignment-ques5

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Welcome=()=>
+{
+return (
+	<h1>Welcome to Coding World</h1>
+	);
+}
+
+const functionExample=()=>
+{
+	return (
+	<Welcome/>
+	);
+}
+
+export default Welcome;
